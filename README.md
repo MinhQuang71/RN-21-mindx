5863115b5e2355a7d1538f25b580fd3975dd99a0
Rachelli.x <macos@Rachellix.local>
2022-09-10 16:39:38 +0700

    test

--- README.md
+++ README.md
@@ -5,6 +5,21 @@ dependencies ->
 
 communication between services
     - API REST-ful server
-    
+
 Session 2: 
-BTVN 
+BTVN:
+    -Tạo 1 file: data/comment.json (array of object)
+1. 
+    -Cài đặt 1 endpoint mới: /comment
+    -Get: read data từ file comments -> trả về cho người dùng
+
+    -Cài đặt 1 endpoint mới: /pokemon
+    -Get: call với endpoint  `https://pokeapi.co/api/v2/pokemon/ditto' va lay data o dang json -> tra ve. Goi y dung thu vien node-fetch 
+
+Session 3:
+GET POST PUT PATCH DELETE
+-get: get some data from some server
+-post: create/adding new data to the server
+-put/patch: changes existed data at the server
+-delete: delete existed data at the server
+
--- app-1.js
+++ app-1.js
@@ -2,11 +2,9 @@ let express = require("express")
 let logger = require("morgan")
 let fs = require("fs") // file stream
 const { nextTick } = require("process")
+const { response } = require("express")
 
 let app = express()
-
-// app.use() // middleware
-// app.use() // 1 endpoint 1 callback
 
 let home_handler = (req,res) => {
   res.json ({
@@ -14,11 +12,11 @@ let home_handler = (req,res) => {
   })
 }
 
-let user_hanlder = (req,res,next) => {
+let user_handler = (req,res,next) => {
   console.log("reach here")
-  fs.readFile('data/users.json','utf8',(err, data) => {
+  fs.readFile('data/user.json','utf8',(err, data) => {
     if (err) {
-      nextTick(err)
+      next(err)
     }
     res.json(
       data
@@ -31,11 +29,47 @@ let error_handler = (err,req,res,next) =>{
     message: err.message
   })
 }
-app.use(
-  logger("dev")
-) 
+let comment_handler = (req, res, next) => {
+  fs.readFile("data/comment.json", "utf8", (err, data) => {
+    if (err) {
+      next(err);
+    }
+    res.json(data);
+  });
+}
+
+function pokemon_handler(req, res, next) {
+  fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+    .then(response => response.json())
+    .then(data => res.json(data))
+    .catch(err => next(err));
+}
+// General middleware
+app.use(express.json()) 
+app.use(logger("dev")) 
 // regex string
-app.use("/user",user_handler)
+// let mw1 = (req, res, next) => {
+//   console.log("Here 1st direction of MW1")
+//   next()
+//   console.log("Here 2nd direction of MW1")
+// }
+
+// let mw2 = (req, res, next) => {
+//   console.log("Here is mw2")
+//   next()
+// }
+
+// let mw3 = (req, res, next) => {
+//   console.log("Here is mw3")
+//   next()
+// }
+
+// app.use(mw3)
+// app.use("/user",mw1,mw2,user_handler)
+app.use("/pokemon", pokemon_handler);
+app.use("/commment", comment_handler);
+app.use("/user", user_handler);
+
 app.use("/",home_handler)
 
 app.use(error_handler)
